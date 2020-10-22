f905466faae603683865264892b1daea114e3e68
Marc Young <mdyoung3@stanford.edu>
2020-10-22 07:05:48 -0600

    SOE-SOEOP-368 - updated message; removed site-init and corrected path to root for aliases

--- .docksal/src_files/soe.aliases.drushrc.php
+++ .docksal/src_files/soe.aliases.drushrc.php
@@ -5,7 +5,7 @@
  * Set the root and site_path values to point to your local site
  */
 $aliases['local'] = array(
-    'root' => '/var/www/soe-clone/build/html',
+    'root' => '/var/www/soe/build/html',
     'uri'  => 'soe.docksal',
     'path-aliases' => array(
         '%dump-dir' => '/tmp',
